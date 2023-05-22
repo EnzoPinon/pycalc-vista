455b531e48ef19ad7d9be47960edf4fd6a0063e7
EnzoPinon <mrlorenzopinon@gmail.com>
2023-05-22 15:21:23 +0800

    started the interface

--- program.py
+++ program.py
@@ -6,5 +6,9 @@ new_ui = UserInterface()
 
 stop_counter = False
 
+print("=============================\nPyCalc\n-Vista Edition-\n\nBy EnzoPinon\n=============================")
+
 while not stop_counter is True:
-    
+    print("**Choices**\n(1) Addition [+]\n(2) Subtraction [-]\n(3) Multiplication [*]\n(4) Division [/]\n(5) Close the application")
+    choice = new_ui.selectionInput()
+    selection = new_ui.selectionValidator(choice)
