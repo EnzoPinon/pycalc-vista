97b825ed6bac1d570602da0a917b4369619e9ee6
EnzoPinon <mrlorenzopinon@gmail.com>
2023-05-22 17:01:38 +0800

    added subtract and multiply method

--- calculate.py
+++ calculate.py
@@ -3,3 +3,11 @@ class Calculator:
     def add(self, num1, num2):
         sum = num1 + num2
         return sum
+    
+    def subtract(self, num1, num2):
+        diff = num1 - num2
+        return diff
+    
+    def multiply(self, num1, num2):
+        produce = num1 * num2
+        return produce
