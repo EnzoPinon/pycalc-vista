7bba1282f99581677085053fef80da79e676a8b3
EnzoPinon <mrlorenzopinon@gmail.com>
2023-05-22 14:43:39 +0800

    started loop counter

--- program.py
+++ program.py
@@ -1,3 +1,10 @@
 from calculate import Calculator
+from interface import UserInterface
 
 calc = Calculator()
+new_ui = UserInterface()
+
+stop_counter = False
+
+while not stop_counter is True:
+    
