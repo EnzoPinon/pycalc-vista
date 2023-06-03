7268698d5ec379b7ba31c7fcd1a4d120f02989b2
EnzoPinon <mrlorenzopinon@gmail.com>
2023-06-03 20:36:49 +0800

    added ability to calculate all at once

--- pycalc.py
+++ pycalc.py
@@ -14,4 +14,20 @@ class pycalc(Calculator):
             result = 'error'
             return result
     
-    
+    def calculate_all(self, num1, num2):
+
+        try:
+            quotient = num1 / num2
+            
+        except ZeroDivisionError:
+            quotient = 'error'
+        
+        try:
+            nth_root = num1 ** 1/num2
+        except ValueError:
+            nth_root = 'error'
+        
+        sum = num1 + num2
+        diff = num1 - num2
+        product = num1 * num2
+        return quotient, nth_root, sum, diff, product
